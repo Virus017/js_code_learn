bf30f34ca68d98be2067e816682a48704ec1161b
Virus017 <chargingreth@gmail.com>
2024-05-02 00:44:29 +0530

    opreators

--- 03_coversion.js
+++ 03_coversion.js
@@ -16,4 +16,38 @@ let isLogedIn = 1
 
 let booleanIsLogedIn = Boolean(isLogedIn)
 
-console.log((booleanIsLogedIn))
+//console.log((booleanIsLogedIn))
+
+// *********** Operations ***********//
+
+let value=4
+let negvalue = -value
+
+//console.log(negvalue)
+
+let str1 = "hello "
+let str2 = "devv"
+let str3 = str1 + str2
+
+console.log(str3)
+
+console.log(1 + "2")
+console.log("3" + 3 + 5)
+console.log(4 + 6 + "4")
+
+let gcount = 300
+++gcount;
+
+console.log(gcount)
+
+let x = 3;
+let y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+let b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
