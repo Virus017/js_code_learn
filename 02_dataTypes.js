c861c20732d9c0d005ea4cd60b5c1955c74ae322
Virus017 <chargingreth@gmail.com>
2024-05-01 15:30:37 +0530

    data types

---
+++ 02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all code as newer version of js
+
+//alert (3+5) //we are using nodejs, not browser. This work on browser 
+
+console.log(5 
+    + 4)  //code readability should be high
+
+let name = "devvrath"
+let age = 2
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//bollean => true/false
+//null => standalone value
+//undefined => value is not asigned
+//symbol => unique
+
+//object
+
+console.log(typeof undefined)
+console.log(typeof null)
