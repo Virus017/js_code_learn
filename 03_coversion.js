65ad2edc6bc884cec44ec31e152edbc5ae88065b
Virus017 <chargingreth@gmail.com>
2024-05-01 17:08:22 +0530

    conversions

---
+++ 03_coversion.js
@@ -0,0 +1,19 @@
+/*let score = "32abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)*/
+
+//"32" => 33
+//"32abc" => NaN(Not a Number)
+// true => 1 false => 0
+
+let isLogedIn = 1
+
+let booleanIsLogedIn = Boolean(isLogedIn)
+
+console.log((booleanIsLogedIn))
