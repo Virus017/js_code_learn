fca343f8ad166b55020f3cec981cae0a2e136e1a
Virus017 <chargingreth@gmail.com>
2024-05-02 15:14:57 +0530

    dataType comparision

---
+++ 04_comparision.js
@@ -0,0 +1,14 @@
+//comparision of diffrent dataTypes
+
+//console.log("3">3)
+//console.log("03">3)
+
+console.log(null>0)  //avoide all these comparision
+console.log(null==0)
+console.log(null>=0)
+
+// ==
+
+console.log("2"===2)
+
+
