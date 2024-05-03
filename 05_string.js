f7b9dc897de2614844d6a243996b1279ddecf115
Virus017 <chargingreth@gmail.com>
2024-05-03 23:45:07 +0530

    about strings

---
+++ 05_string.js
@@ -0,0 +1,40 @@
+const name = "devv"
+const repCount = 2
+
+// console.log(name + repoCount + "value") //do not use this
+
+console.log(`My name is ${name} and my repositories are ${repCount}`)
+
+const newGame = new String('virus23-noob-pro')
+
+// console.log(newGame[2])
+// console.log(newGame.__proto__)
+
+// console.log(newGame.length)
+// console.log(newGame.toUpperCase())
+
+console.log(newGame.charAt(0))
+console.log(newGame.indexOf("v"))
+
+const newString = newGame.substring(0,4)
+console.log(newString)
+
+const anotherString = newString.slice(-5,4)
+console.log(anotherString)
+
+const secString = "  hello   "
+console.log(secString)
+console.log(secString.trim())
+
+const url = "https://devv23.com/hello20%world"
+
+console.log(url.replace('20%','-'))
+console.log(url.includes('hello'))
+
+console.log(newGame.split('-'))  //it split and convert string to arrays
+
+
+// GYannn.............
+
+
+
