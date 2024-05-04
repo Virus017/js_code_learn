a6825ea388c9b3c08a22ef967c3875ebbb69aa79
Virus017 <chargingreth@gmail.com>
2024-05-04 15:04:03 +0530

    numbers and maths

---
+++ 06_no_and_math.js
@@ -0,0 +1,34 @@
+const score = 200
+//console.log(score);
+
+const balance = new Number(499)
+//console.log(balance);
+
+//console.log(balance.toString());
+//console.log(balance.toFixed(1));
+
+const otherNumber = 156.5825
+console.log(otherNumber.toPrecision(4));
+
+const bigNumber = 1000000
+//console.log(bigNumber.toLocaleString("en-IN"));
+
+// ****************MATHS********************
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(45.56))
+// console.log(Math.ceil(2.1))
+// console.log(Math.floor(2.7))
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+console.log(Math.floor((Math.random()*10)) + 1); //Adding 1 will result in the min value to be 1
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1) + min))
+// setting max and min range 
+
